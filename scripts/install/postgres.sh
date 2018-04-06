cc367e1f4de2a6da467ced0ee37e6b01c3e57665
Josh Pollara <joshpollara@gmail.com>
2018-04-06 18:39:23 +0200

    wait for postgres + grafana db setup

--- scripts/install/postgres.sh
+++ scripts/install/postgres.sh
@@ -1,6 +1,14 @@
 #! /bin/sh
 set -e
 
+while true; do
+  if nc -z -v -w 1 postgres 5432; then
+    break
+  else
+    sleep 1
+  fi
+done
+
 cli_args="-h ${POSTGRES_HOST:-postgres} -U ${POSTGRES_USER:-postgres} -p ${POSTGRES_PORT:-5432} -v ON_ERROR_STOP=1"
 
 res=$(psql $cli_args -Atc "select version from app.version;" || echo '') 2>/dev/null
@@ -13,6 +21,13 @@ if [ "$res" = "" ]; then
 
   psql $cli_args -f ../psql/hub/main.sql
 
-  psql $cli_args -c 'create database grafana;'
+fi
+
+res=$(psql $cli_args -Atc "select datname from pg_database where datistemplate = false and datname='grafana';")
+if [ "$res" = "" ]; then
+
+  psql $cli_args -c "CREATE USER grafana WITH PASSWORD '${GRAFANA_DB_PASSWORD:-grafana}';";
+  psql $cli_args -c "CREATE DATABASE grafana;"
+  psql $cli_args -c "GRANT ALL PRIVILEGES ON DATABASE grafana TO grafana;"
 
 fi
