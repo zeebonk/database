d4fb8dc59a69b133499a98a477b7f7efd7fe3246
Steve Peak <steve@stevepeak.net>
2018-04-06 16:27:51 +0200

    wip(db): move to schemas instread of databases

--- scripts/install/postgres.sh
+++ scripts/install/postgres.sh
@@ -3,17 +3,15 @@ set -e
 
 cli_args="-h ${POSTGRES_HOST:-postgres} -U ${POSTGRES_USER:-postgres} -p ${POSTGRES_PORT:-5432} -v ON_ERROR_STOP=1"
 
-res=$(psql $cli_args -Atc "select datname from pg_database where datistemplate = false and datname='app';")
+res=$(psql $cli_args -Atc "select version from app.version;" || echo '') 2>/dev/null
 
 if [ "$res" = "" ]; then
 
   echo '===> Setting up PostgreSQL'
 
-  psql $cli_args -c 'create database app;'
-  psql $cli_args -d app -f ../psql/app/main.sql
+  psql $cli_args -f ../psql/app/main.sql
 
-  psql $cli_args -c 'create database hub;'
-  psql $cli_args -d hub -f ../psql/hub/main.sql
+  psql $cli_args -f ../psql/hub/main.sql
 
   psql $cli_args -c 'create database grafana;'
 
