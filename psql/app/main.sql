a90ed9323bd7f9c25e9dd68a4d950276b490e8a2
Josh Pollara <joshpollara@gmail.com>
2018-04-07 08:57:35 +0200

    if not exists

--- psql/app/main.sql
+++ psql/app/main.sql
@@ -1,9 +1,10 @@
-create schema app_public;
+create schema if not exists app_public;
 set search_path to app_public;
 
-create extension "pgcrypto" with schema app_public;
-create extension "uuid-ossp" with schema app_public;
-create extension "citext" with schema app_public;
+\c app
+create extension if not exists "pgcrypto" with schema app_public;
+create extension if not exists "uuid-ossp" with schema app_public;
+create extension if not exists "citext" with schema app_public;
 
 create table version (version text);
 insert into version values ('0.0.1');
--- psql/hub/main.sql
+++ psql/hub/main.sql
@@ -1,8 +1,9 @@
-create schema hub_public;
+create schema if not exists hub_public;
 set search_path to hub_public;
 
-create extension "uuid-ossp" with schema hub_public;
-create extension "citext" with schema hub_public;
+\c hub
+create extension if not exists "uuid-ossp" with schema hub_public;
+create extension if not exists "citext" with schema hub_public;
 
 create table version (version text);
 insert into version values ('0.0.1');
