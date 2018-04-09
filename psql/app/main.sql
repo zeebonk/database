5a4f5f22a18160ae0a174e3addaf76b505af3e79
Steve Peak <steve@stevepeak.net>
2018-04-09 15:59:59 +0200

    fix(db): merge conflict

--- psql/app/main.sql
+++ psql/app/main.sql
@@ -1,18 +1,9 @@
-<<<<<<< Updated upstream
-create schema if not exists app_public;
-set search_path to app_public;
-
-create extension if not exists "pgcrypto" with schema app_public;
-create extension if not exists "uuid-ossp" with schema app_public;
-create extension if not exists "citext" with schema app_public;
-=======
 CREATE SCHEMA app_public;
 SET search_path to app_public;
 
 CREATE EXTENSION "pgcrypto" with schema app_public;
 CREATE EXTENSION "uuid-ossp" with schema app_public;
 CREATE EXTENSION "citext" with schema app_public;
->>>>>>> Stashed changes
 
 CREATE TABLE version (version text);
 INSERT INTO version values ('0.0.1');
