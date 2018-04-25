fed19fd1303b1ed53ea0542ddc766bc5bc8ebbfc
Benjie Gillam <benjie@jemjie.com>
2018-04-25 09:32:34 +0100

    Use join table for team permissions

--- psql/tables/teams.sql
+++ psql/tables/teams.sql
@@ -3,24 +3,18 @@ CREATE TABLE teams(
   owner_uuid              uuid references owners on delete cascade not null,
   name                    title not null,
   service                 service not null default 'github'::service,
-  service_id              citext CHECK (LENGTH(service_id) < 45) not null,
-  permissions             uuid[]
+  service_id              citext CHECK (LENGTH(service_id) < 45) not null
 );
 COMMENT on column teams.owner_uuid is 'The GitHub Organization that this team belong to.';
 COMMENT on column teams.name is 'The title of the Team.';
 COMMENT on column teams.service_id is 'The GitHub unique service id.';
-COMMENT on column teams.permissions is 'A list of permissions this team has permission to.';
 
 CREATE UNIQUE INDEX teams_service_ids on teams (service, service_id);
 
 CREATE INDEX teams_owner_uuid_fk on teams (owner_uuid);
 
-CREATE TRIGGER _100_insert_assert_permissions_exist before insert on teams
-  for each row
-  when (array_length(new.permissions, 1) > 0)
-  execute procedure assert_permissions_exist();
-
-CREATE TRIGGER _100_update_assert_permissions_exist before update on teams
-  for each row
-  when (new.permissions is distinct from old.permissions and array_length(new.permissions, 1) > 0)
-  execute procedure assert_permissions_exist();
+CREATE TABLE team_permissions (
+  team_uuid               uuid not null references teams on delete cascade,
+  permission_uuid         uuid not null references permissions on delete restrict,
+  PRIMARY KEY (team_uuid, permission_uuid)
+);
