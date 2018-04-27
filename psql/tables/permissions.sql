66d4b819cf664cb2591b6e809f77c32ecd8ce573
Benjie Gillam <benjie@jemjie.com>
2018-04-27 08:31:18 +0100

    Permissions check

--- psql/tables/permissions.sql
+++ psql/tables/permissions.sql
@@ -7,8 +7,17 @@ COMMENT on column permissions.title is 'Short description of the permission.';
 
 CREATE FUNCTION assert_permissions_exist() returns trigger as $$
 begin
-  -- TODO check one dimention
-  -- TODO  that all exist in permissions table
+  if array_ndims(NEW.permissions) > 1 then
+    raise exception 'Invalid permissions, expected one-dimensional array' using errcode = 'SECPD';
+  end if;
+  if exists(
+    select 1
+    from unnest(NEW.permissions) puuid
+    left join permissions on (puuid = permissions.uuid)
+    where permissions.uuid is null
+  ) then
+    raise exception 'Invalid permissions, permission unrecognised' using errcode = 'SECPX';
+  end if;
  return new;
 end;
 $$ language plpgsql;
