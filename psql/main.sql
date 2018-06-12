cbe4f3e8b05a034135286f75cb792f8ef3b6cb1a
Steve Peak <steve@stevepeak.net>
2018-06-12 20:19:29 +0200

    revert version.id

--- psql/main.sql
+++ psql/main.sql
@@ -8,7 +8,6 @@ CREATE SCHEMA app_private;
 SET search_path to app_public, app_private, public;
 
 CREATE TABLE app_private.version (
-  id                  serial PRIMARY KEY,
   version             text CHECK (version ~ '^\d+\.\d+\.\d+$') primary key
 );
 INSERT INTO app_private.version values ('0.0.1');
