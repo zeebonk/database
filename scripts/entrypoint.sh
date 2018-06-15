1346fb9b743bb079f2fe2f4c96c60e6d387e67f7
Steve Peak <steve@stevepeak.net>
2018-06-15 15:43:32 +0200

    move schema into asset dir

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -20,8 +20,9 @@ if [ "$POSTGRES" = "yes" ]; then
   ./scripts/install/postgres.sh
 fi
 
-echo '===> Adding Release Story'
+echo '===> Adding Release Assets'
 mv /deploy.json /asyncy/config/deploy.json
+mv /assets/schemas/config.json /asyncy/config/asyncy-schema.json
 
 # Run
 echo '===> Running Flask server'
