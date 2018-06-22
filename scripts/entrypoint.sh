955acec9d26231a89fb7c812fd7593feb829d68a
Steve Peak <steve@stevepeak.net>
2018-06-22 11:11:36 +0200

    update exec

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,20 +1,6 @@
 #! /bin/sh
 set -e
 
-echo '
-
-             _____  __     __  _   _    _____  __     __
-    /\      / ____| \ \   / / | \ | |  / ____| \ \   / /
-   /  \    | (___    \ \_/ /  |  \| | | |       \ \_/ /
-  / /\ \    \___ \    \   /   |     | | |        \   /
- / /  \ \   ____) |    | |    | |\  | | |____     | |
-/_/    \_\ |_____/     |_|    |_| \_|  \_____|    |_|
-
-            Write stories, then code.
-
-'
-
-
 # check $POSTGRES only in Alpha, since its used in the Asyncy Hub
 if [ "$POSTGRES" = "yes" ]; then
   ./scripts/install/postgres.sh
@@ -22,4 +8,4 @@ fi
 
 # Run
 echo '===> Running Server'
-python ./server/app.py
+python -m server.app
