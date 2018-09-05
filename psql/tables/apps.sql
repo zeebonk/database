1ebffd1fbd19df5895cfd71c7cf7caec0af52f70
Steve Peak <steve@stevepeak.net>
2018-09-05 11:51:59 +0200

    apps are not required to be backed by a remote repo

--- psql/tables/apps.sql
+++ psql/tables/apps.sql
@@ -1,7 +1,7 @@
 CREATE TABLE apps(
   uuid                    uuid default uuid_generate_v4() primary key,
   organization_uuid       uuid references organizations on delete cascade not null,
-  repo_uuid               uuid references repos on delete cascade not null,
+  repo_uuid               uuid references repos on delete cascade,
   name                    title not null,
   timestamp               timestamptz not null,
   maintenance             boolean default false not null
