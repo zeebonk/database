a404ad7246bc0e539a21b5f27f15002c162a9690
Steve Peak <steve@stevepeak.net>
2018-09-05 09:55:04 +0200

    add maintenance mode column

--- psql/tables/apps.sql
+++ psql/tables/apps.sql
@@ -2,7 +2,8 @@ CREATE TABLE apps(
   uuid                    uuid default uuid_generate_v4() primary key,
   organization_uuid       uuid references organizations on delete cascade not null,
   repo_uuid               uuid references repos on delete cascade not null,
-  name                    title not null
+  name                    title not null,
+  maintenance             boolean default false not null
 );
 COMMENT on table apps is 'Owned by an org, an App is a group of Repos that make up an application.';
 
