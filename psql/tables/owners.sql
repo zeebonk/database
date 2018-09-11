c452335911315990a3912b0b1594fac4fb7742ce
Steve Peak <steve@stevepeak.net>
2018-09-11 15:24:56 +0200

    Update owners.sql

--- psql/tables/owners.sql
+++ psql/tables/owners.sql
@@ -41,3 +41,73 @@ CREATE TABLE owner_secrets (
   email_verification_code varchar(30)
 );
 COMMENT on column owner_secrets.oauth_token is 'Stored as an encrypted string';
+
+CREATE or replace FUNCTION createOwnerByLogin(
+    service app_public.git_service,
+    service_id text,
+    username username,
+    name text,
+    email email,
+    oauth_token text
+) RETURNS uuid AS $$
+  #variable_conflict use_variable
+  DECLARE _owner_uuid uuid DEFAULT NULL;
+  DECLARE _token_uuid uuid DEFAULT NULL;
+  BEGIN
+
+    -- TODO IF (service, username) conflict THEN need to truncate the other username
+    -- TODO IF (service, service_id) conflict THEN need to update the username
+
+    SELECT uuid into _owner_uuid
+      FROM owners
+      WHERE service=service
+        AND username=username
+      LIMIT 1;
+
+    IF _owner_uuid IS NOT NULL THEN
+
+      -- update their oauth token
+      UPDATE owner_secrets
+        SET oauth_token=oauth_token
+        WHERE owner_uuid=_owner_uuid;
+
+      SELECT uuid into _token_uuid
+        FROM tokens
+        WHERE owner_uuid=_owner_uuid
+          AND type='LOGIN'
+        LIMIT 1;
+
+    ELSE
+      
+      INSERT INTO owners (service, service_id, is_user, username, name)
+        VALUES (service, service_id, true, username, name)
+        RETURNING uuid into _owner_uuid;
+
+      INSERT INTO owner_emails (owner_uuid, email, is_verified)
+        VALUES (_owner_uuid, email, true);
+      
+      UPDATE owner_secrets
+        SET oauth_token=oauth_token
+        WHERE owner_uuid=_owner_uuid;
+
+    END IF;
+
+    IF _token_uuid IS NULL THEN
+
+      INSERT INTO tokens (owner_uuid, type, name, expires)
+        VALUES (_owner_uuid, 'LOGIN', 'CLI Login', current_timestamp + '3 months'::interval)
+        RETURNING uuid into _token_uuid;
+
+    ELSE
+
+      UPDATE tokens
+        SET expires=(current_timestamp + '3 months'::interval)
+        WHERE owner_uuid=_owner_uuid
+          AND type='LOGIN';
+
+    END IF;
+
+    RETURN _token_uuid;
+    
+  END;
+$$ LANGUAGE plpgsql VOLATILE SET search_path FROM CURRENT;
