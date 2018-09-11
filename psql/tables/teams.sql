68f56e38327c59024d878b1e580adb05332f6295
Steve Peak <steve@stevepeak.net>
2018-09-11 21:20:10 +0200

    update teams to link orgs and not gh backed #25

--- psql/tables/teams.sql
+++ psql/tables/teams.sql
@@ -1,17 +1,13 @@
 CREATE TABLE teams(
   uuid                    uuid default uuid_generate_v4() primary key,
-  owner_uuid              uuid references owners on delete cascade not null,
+  organization_uuid       uuid references organizations on delete cascade not null,
   name                    title not null,
-  service                 git_service not null default 'github'::git_service,
-  service_id              citext CHECK (LENGTH(service_id) < 45) not null
+  UNIQUE (organization_uuid, name)
 );
-COMMENT on column teams.owner_uuid is 'The GitHub Organization that this team belong to.';
+COMMENT on column teams.organization_uuid is 'The Organization that this team belong to.';
 COMMENT on column teams.name is 'The title of the Team.';
-COMMENT on column teams.service_id is 'The GitHub unique service id.';
 
-CREATE UNIQUE INDEX teams_service_ids on teams (service, service_id);
-
-CREATE INDEX teams_owner_uuid_fk on teams (owner_uuid);
+CREATE INDEX teams_owner_uuid_fk on teams (organization_uuid);
 
 CREATE TABLE team_permissions (
   team_uuid               uuid not null references teams on delete cascade,
