f0f288dcee99351f7d0ed9250926157690e6d8b0
Benjie Gillam <benjie@jemjie.com>
2018-09-12 14:45:51 +0100

    Move to file per table

--- psql/permissions/main.sql
+++ psql/permissions/main.sql
@@ -1,5 +1,1 @@
-ALTER TABLE apps ENABLE ROW LEVEL SECURITY;
-
-CREATE POLICY select_own ON apps FOR SELECT USING (owner_uuid = current_owner_uuid());
-CREATE POLICY select_organization ON apps FOR SELECT USING (organization_uuid = ANY (current_owner_organization_uuids()));
-GRANT SELECT ON apps TO asyncy_visitor;
+\ir ./apps.sql
