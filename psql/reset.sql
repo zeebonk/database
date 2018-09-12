8c2592d6a1baaef2c55057891f6ae00cb3f25bb4
Benjie Gillam <benjie@jemjie.com>
2018-09-12 13:47:32 +0100

    Create roles

--- psql/reset.sql
+++ psql/reset.sql
@@ -2,9 +2,18 @@ DROP SCHEMA IF EXISTS app_public CASCADE;
 DROP SCHEMA IF EXISTS app_private CASCADE;
 DROP SCHEMA IF EXISTS app_hidden CASCADE;
 DROP SCHEMA IF EXISTS app_jobs CASCADE;
+DROP ROLE IF EXISTS asyncy_authenticator;
+DROP ROLE IF EXISTS asyncy_visitor;
 
 DROP EXTENSION IF EXISTS "pgcrypto";
 DROP EXTENSION IF EXISTS "uuid-ossp";
 DROP EXTENSION IF EXISTS "citext";
+
+CREATE ROLE asyncy_authenticator WITH LOGIN PASSWORD 'PLEASE_CHANGE_ME' NOINHERIT;
+CREATE ROLE asyncy_visitor;
+GRANT asyncy_visitor to asyncy_authenticator;
+
+REVOKE ALL ON DATABASE asyncy FROM PUBLIC;
+GRANT CONNECT ON DATABASE asyncy TO asyncy_authenticator;
 
 \ir ./main.sql
