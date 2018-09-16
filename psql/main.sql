e6fd1ccd1aedb30f008e956b4e9212411e38d54c
Steve Peak <steve@stevepeak.net>
2018-09-16 17:41:16 +0200

    add roles to startup

--- psql/main.sql
+++ psql/main.sql
@@ -6,6 +6,21 @@ CREATE SCHEMA app_public;
 CREATE SCHEMA app_hidden;
 CREATE SCHEMA app_private;
 SET search_path to app_public, app_hidden, app_private, public;
+
+DO $$
+BEGIN
+  IF NOT EXISTS(SELECT 1 FROM pg_roles WHERE rolname = 'asyncy_authenticator') THEN
+    CREATE ROLE asyncy_authenticator WITH LOGIN PASSWORD 'PLEASE_CHANGE_ME' NOINHERIT;
+  END IF;
+  IF NOT EXISTS(SELECT 1 FROM pg_roles WHERE rolname = 'asyncy_visitor') THEN
+    CREATE ROLE asyncy_visitor;
+  END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+GRANT asyncy_visitor to asyncy_authenticator;
+GRANT CONNECT ON DATABASE postgres TO asyncy_authenticator;
+
 GRANT USAGE ON SCHEMA app_public TO asyncy_visitor;
 GRANT USAGE ON SCHEMA app_hidden TO asyncy_visitor;
 
