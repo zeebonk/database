699f1e00b16735c69600874468c36964a128422e
Steve Peak <steve@stevepeak.net>
2018-09-16 18:57:45 +0200

    update hostname length

--- psql/types.sql
+++ psql/types.sql
@@ -7,7 +7,7 @@ CREATE DOMAIN username as text
   CHECK (LENGTH(VALUE) <= 40 AND VALUE ~ '^[\w\-]+$' );
 
 CREATE DOMAIN hostname as text
-  CHECK ( LENGTH(VALUE) > 7 AND LENGTH(VALUE) < 25 AND VALUE ~ '^((\*|[a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$' );
+  CHECK ( LENGTH(VALUE) > 3 AND LENGTH(VALUE) < 255 AND VALUE ~ '^((\*|[a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$' );
 
 CREATE DOMAIN email AS citext
   CHECK ( LENGTH(VALUE) >= 5 AND LENGTH(VALUE) <= 512 AND VALUE ~ '^[^@]+@[^@]+\.[^@]+$' );
