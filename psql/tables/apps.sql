ce67e3928aba5156c4417edd259a16036381ff48
Steve Peak <steve@stevepeak.net>
2018-09-26 10:42:36 +0200

    [organize] triggers

--- psql/tables/apps.sql
+++ psql/tables/apps.sql
@@ -27,37 +27,3 @@ COMMENT on column app_dns.hostname is 'A full hostname entry such as foobar.asyn
 COMMENT on column app_dns.is_validated is 'If dns resolves properly from registry.';
 
 CREATE INDEX app_dns_app_uuid_fk on app_dns (app_uuid);
-
-CREATE FUNCTION apps_create_dns() returns trigger as $$
-  DECLARE dns hostname default null;
-  BEGIN
-
-    BEGIN
-      INSERT INTO app_dns (hostname, app_uuid, is_validated)
-        VALUES (convert_to_hostname(new.name), new.uuid, true)
-        ON CONFLICT (hostname) DO NOTHING
-        RETURNING hostname into dns;
-
-      IF dns IS NOT NULL THEN
-        RETURN new;
-      END IF;
-    EXCEPTION WHEN check_violation THEN
-      -- Do nothing
-    END;
-
-    LOOP
-      INSERT INTO app_dns (hostname, app_uuid, is_validated)
-        VALUES (generate_awesome_word()::hostname, new.uuid, true)
-        ON CONFLICT (hostname) DO NOTHING
-        RETURNING hostname into dns;
-
-      IF dns IS NOT NULL THEN
-        RETURN new;
-      END IF;
-    END LOOP;
-
-  END;
-$$ language plpgsql security definer SET search_path FROM CURRENT;
-
-CREATE TRIGGER _101_apps_create_dns after insert on apps
-  for each row execute procedure apps_create_dns();
---
+++ psql/triggers/main.sql
@@ -0,0 +1,3 @@
+\ir create_organization.sql
+\ir must_have_one_admin.sql
+\ir create_app_default_dns.sql
