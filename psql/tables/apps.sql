b34fd5247b32c5dd4d4bc179d1477c37e6603057
Jude Pereira <contact@judepereira.com>
2018-12-11 16:11:12 +0530

    Raise an notification on "releases" when an app is deleted/set to maintenance.

--- psql/tables/apps.sql
+++ psql/tables/apps.sql
@@ -27,3 +27,14 @@ COMMENT on column app_dns.hostname is 'A full hostname entry such as foobar.asyn
 COMMENT on column app_dns.is_validated is 'If dns resolves properly from registry.';
 
 CREATE INDEX app_dns_app_uuid_fk on app_dns (app_uuid);
+
+CREATE FUNCTION app_updated_notify() returns trigger as $$
+  begin
+    perform pg_notify('release', cast(old.uuid as text));
+  end;
+$$ language plpgsql security definer SET search_path FROM CURRENT;
+
+CREATE TRIGGER _100_app_updated_notify() after update on apps
+  for each row
+  when (old.maintenance is distinct from new.maintenance or new.deleted=true)
+  execute procedure app_updated();
